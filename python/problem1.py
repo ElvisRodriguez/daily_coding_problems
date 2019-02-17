208569c9646ad93be8643864ad57f38e5943b47f
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-02-17 18:41:29 +0000

    Optimized problem 1

--- python/problem1.py
+++ python/problem1.py
@@ -10,21 +10,23 @@ Assumptions: cannot use the same value twice, i.e. given [4, 5, 6, 7] and
              k of 8, it is not true to say 4 and 4 make this true.
 '''
 
-
+#Runtime: O(n) where n is the size of the number array.
 def two_sum(nums, k):
+    #Storing each number in a hash table will allow for O(1) lookup, meaning we
+    #only need to iterate through the number array once.
     numbers = {}
     for i in range(len(nums)):
-        numbers[nums[i]] = i
         difference = k - nums[i]
-        if difference in numbers and nums.index(difference) != i:
+        if difference in numbers:
             return True
+        else:
+            numbers[nums[i]] = i
     return False
 
-
 if __name__ == '__main__':
-    # Testing
-    nums = [[10, 15, 3, 7], [2, 7, 4, 4], [5, 6, 4]]
-    k_values = [17, 8, 8]
+    #Testing example arrays and edge case
+    nums = [[10,15,3,7], [2,7,4,4], [2,7,4]]
+    k_values = [17,8,8]
     for i in range(len(k_values)):
         print('Given {nums} and k of {k}:'.format(nums=nums[i], k=k_values[i]))
         print('Result is {result}'.format(
